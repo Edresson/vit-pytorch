8a0f914515be0de7244ebc3585aa45760b9d4756
Edresson <edresson1@gmail.com>
2021-02-23 03:08:21 -0300

    add speech transformer

--- vit_pytorch/vit_pytorch.py
+++ vit_pytorch/vit_pytorch.py
@@ -113,15 +113,51 @@ class ViT(nn.Module):
     def forward(self, img, mask = None):
         x = self.to_patch_embedding(img)
         b, n, _ = x.shape
-
         cls_tokens = repeat(self.cls_token, '() n d -> b n d', b = b)
         x = torch.cat((cls_tokens, x), dim=1)
         x += self.pos_embedding[:, :(n + 1)]
         x = self.dropout(x)
-
         x = self.transformer(x, mask)
-
         x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
-
+        # exit()
         x = self.to_latent(x)
         return self.mlp_head(x)
+
+class SpT(nn.Module):
+    def __init__(self, *, input_dim, num_max_patches, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', dim_head = 64, dropout = 0., emb_dropout = 0.):
+        super().__init__()
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+        self.linear = nn.Linear(input_dim, dim)
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_max_patches + 1, dim))
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
+        
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.pool = pool
+        self.to_latent = nn.Identity()
+
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, num_classes)
+        )
+
+    def forward(self, x, mask = None):
+        x_shape = x.shape
+        b, n = x_shape[:2]
+        x = x.reshape(b, n, -1)
+
+        x = self.linear(x)
+        cls_tokens = repeat(self.cls_token, '() n d -> b n d', b = b)
+        
+        x = torch.cat((cls_tokens, x), dim=1)
+        x += self.pos_embedding[:, :(n + 1)]
+        x = self.dropout(x)
+       
+        x = self.transformer(x, mask)
+        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+        x = self.to_latent(x)
+        out = self.mlp_head(x)
+        return out
